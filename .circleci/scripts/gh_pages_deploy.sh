f8ab9b8b2f49f725ad8371ba3d55dd1ab7a6c79f
Jason Hagglund <jhagglund@doximity.com>
2019-02-08 12:35:06 -0800

    The if loop is dump, control branch execution at ci config level

--- .circleci/scripts/gh_pages_deploy.sh
+++ .circleci/scripts/gh_pages_deploy.sh
@@ -3,26 +3,24 @@
 set -x
 set -e
 
-if [[ $CIRCLE_BRANCH = "master" ]]; then
-  git config --global user.email $GH_EMAIL
-  git config --global user.name $GH_NAME
+git config --global user.email $GH_EMAIL
+git config --global user.name $GH_NAME
 
-  git clone $CIRCLE_REPOSITORY_URL out
+git clone $CIRCLE_REPOSITORY_URL out
 
-  cd out
-  git checkout gh-pages || git checkout --orphan gh-pages
-  git rm -rf .
-  cd ..
+cd out
+git checkout gh-pages || git checkout --orphan gh-pages
+git rm -rf .
+cd ..
 
-  npm run build
+npm run build
 
-  cp -a build/. out/.
+cp -a build/. out/.
 
-  mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
-  cd out
+mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
+cd out
 
-  git add -A
-  git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+git add -A
+git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
 
-  git push origin gh-pages
-fi
+git push origin gh-pages
